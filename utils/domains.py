e34850cf3c2f8ffa8dab9020895ed37fb12ca40e
TPGamesNL <pet.teun.03@gmail.com>
2024-02-22 21:46:12 +0100

    Fix some bugs in domains.py get_san_names

--- utils/domains.py
+++ utils/domains.py
@@ -11,13 +11,13 @@ def get_san_names(domain):
     with socket.create_connection((domain, 443), timeout=2) as sock:
         with context.wrap_socket(sock, server_hostname=domain) as ssock:
             cert = ssock.getpeercert()
-            san = {}
+            sans = {}
             for type_, san in cert['subjectAltName']:
-                if type_ in san:
-                    san[type_] = []
-                san[type_].append(san)
+                if type_ not in sans:
+                    sans[type_] = []
+                sans[type_].append(san)
             
-            return san['DNS']
+            return sans['DNS']
 
 def get_registered_domain(hostname):
     """
