34aa0912341a53a693f079bb5d74e7aa42d2d9ce
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-02-28 13:52:20 +0200

    Added Ruff (#14)
    * Added initial ruff settings
    
    * Added new rules
    
    * Reformatted api.py
    
    * Removed unused file
    
    * Updated ruff.toml rules
    
    * Reformatted api.py
    
    * Updated some rules and refactored detection and parsing files
    
    * Added comments to ruff.toml
    
    * Refactored domains.py
    
    * Updated classifiers.py
    
    * Updated ruff.toml and refactored classifiers.py and custom_logger.py
    
    * Refactored proxy_getter.py
    
    * Formatted regions_detection.py
    
    * Formatted reverse_image_search.py
    
    * Formatted sessions.py
    
    * Formatted timing.py
    
    * Formatted utils.py
    
    * Formatted files in engine and utils.py
    
    * Update README.md
    
    * Update README.md
    
    * Fix typo
    
    * Updated description and selected rules in ruff.toml
    
    ---------
    
    Co-authored-by: TPGamesNL <pet.teun.03@gmail.com>

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -1,12 +1,15 @@
 import logging
 import os
 import time
+
 import requests
 
-class CustomLogger():
+
+class CustomLogger:
     """
     Singleton instance for logging, use `CustomLogger().main_logger` to get the instance.
     """
+
     _instance = None
     main_logger = None
     loggerid = None
@@ -18,35 +21,39 @@ class CustomLogger():
         return self._instance
 
     def _setup(self):
-        if not os.path.exists('log'):
-            os.makedirs('log')
+        if not os.path.exists("log"):
+            os.makedirs("log")
         self.loggerid = self.get_ip()
 
         self.main_logger = logging.getLogger("mainlogger")
         self.main_logger.setLevel(logging.DEBUG)
 
-        formatter_c = logging.Formatter('%(asctime)s  [\033[93m%(pathname)s:%(lineno)d\033[0m]  %(levelname)s: %(message)s')
-        formatter_f = logging.Formatter('%(asctime)s  [%(pathname)s:%(lineno)d]  %(levelname)s: %(message)s')
+        formatter_c = logging.Formatter(
+            "%(asctime)s  [\033[93m%(pathname)s:%(lineno)d\033[0m]  %(levelname)s: %(message)s"
+        )
+        formatter_f = logging.Formatter(
+            "%(asctime)s  [%(pathname)s:%(lineno)d]  %(levelname)s: %(message)s"
+        )
 
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setLevel(logging.INFO)
-        consoleHandler.setFormatter(formatter_c)
-        self.main_logger.addHandler(consoleHandler)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(formatter_c)
+        self.main_logger.addHandler(console_handler)
 
-        fileHandler = logging.FileHandler(f"log/python-{time.time()}.log", mode='w')
-        fileHandler.setLevel(logging.INFO)
-        fileHandler.setFormatter(formatter_f)
-        self.main_logger.addHandler(fileHandler)
+        file_handler = logging.FileHandler(f"log/python-{time.time()}.log", mode="w")
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter_f)
+        self.main_logger.addHandler(file_handler)
 
-        fileHandlerE = logging.FileHandler(f"log/error-{time.time()}.log", mode='w')
-        fileHandlerE.setLevel(logging.ERROR)
-        fileHandlerE.setFormatter(formatter_f)
-        self.main_logger.addHandler(fileHandlerE)
-        
-        fileHandlerD = logging.FileHandler(f"log/debug-{time.time()}.log", mode='w')
-        fileHandlerD.setLevel(logging.DEBUG)
-        fileHandlerD.setFormatter(formatter_f)
-        self.main_logger.addHandler(fileHandlerD)
+        file_handler_error = logging.FileHandler(f"log/error-{time.time()}.log", mode="w")
+        file_handler_error.setLevel(logging.ERROR)
+        file_handler_error.setFormatter(formatter_f)
+        self.main_logger.addHandler(file_handler_error)
+
+        file_handler_debug = logging.FileHandler(f"log/debug-{time.time()}.log", mode="w")
+        file_handler_debug.setLevel(logging.DEBUG)
+        file_handler_debug.setFormatter(formatter_f)
+        self.main_logger.addHandler(file_handler_debug)
 
         return self.main_logger
 
--- utils/region_detection.py
+++ utils/region_detection.py
@@ -1,15 +1,18 @@
-import cv2
+import math
 import os.path
+import random
+from enum import Enum
+
+import cv2
 import numpy as np
 import scipy.stats as ss
 from pywt import dwt2
-import random
-import math
-from enum import Enum
 
 # Set up logging
 from utils.custom_logger import CustomLogger
+
 main_logger = CustomLogger().main_logger
+
 
 class RegionData:
     region = None
@@ -29,7 +32,25 @@ class RegionData:
     energy = None
     occupied_bins = None
 
-    def __init__(self, region, index, x, y, unique_colors_count, pct, hierarchy, invert, mean, std, skew, kurtosis, entropy, otsu, energy, occupied_bins):
+    def __init__(
+        self,
+        region,
+        index,
+        x,
+        y,
+        unique_colors_count,
+        pct,
+        hierarchy,
+        invert,
+        mean,
+        std,
+        skew,
+        kurtosis,
+        entropy,
+        otsu,
+        energy,
+        occupied_bins,
+    ):
         self.region = region
         self.index = index
         self.x = x
@@ -47,7 +68,8 @@ class RegionData:
         self.energy = energy
         self.occupied_bins = occupied_bins
 
-def _count_colours(image : cv2.typing.MatLike) -> tuple[int, float]:
+
+def _count_colours(image: cv2.typing.MatLike) -> tuple[int, float]:
     """
     Get the number of unique colours and the percentage of the primary colour in the given image.
     """
@@ -56,14 +78,22 @@ def _count_colours(image : cv2.typing.MatLike) -> tuple[int, float]:
     flattend_image = image.reshape(-1, image.shape[-1])
 
     # Get the unique colors and the number of times each appears in the image
-    unique_colors, unique_colors_pixels = np.unique(flattend_image, axis = 0, return_counts = True) 
+    unique_colors, unique_colors_pixels = np.unique(flattend_image, axis=0, return_counts=True)
 
-    primary_color_percentage = np.amax(unique_colors_pixels, initial = 0) / max(np.sum(unique_colors_pixels), 1) * 100
+    primary_color_percentage = (
+        np.amax(unique_colors_pixels, initial=0) / max(np.sum(unique_colors_pixels), 1) * 100
+    )
 
     return len(unique_colors), primary_color_percentage
 
 
-def _draw_regions(image: cv2.typing.MatLike, image_path: str, regions_data: list[RegionData], highlight_name: str, subregion_draw = False):
+def _draw_regions(
+    image: cv2.typing.MatLike,
+    image_path: str,
+    regions_data: list[RegionData],
+    highlight_name: str,
+    subregion_draw=False,
+):
     """
     Draw the detected regions on the originial image.
     """
@@ -78,12 +108,14 @@ def _draw_regions(image: cv2.typing.MatLike, image_path: str, regions_data: list
         y = region_data.y
 
         color_int = random.randint(0, 2)
-        colors = [(0,0,255), (0,255,0), (255,0,0)]
+        colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
         color = colors[color_int]
 
-        flip = (random.randint(0, 1) == 1)
+        flip = random.randint(0, 1) == 1
 
-        cv2.rectangle(draw_image, (x - 5, y - 5), (x + region_width - 5, y + region_height - 5), color, 1)
+        cv2.rectangle(
+            draw_image, (x - 5, y - 5), (x + region_width - 5, y + region_height - 5), color, 1
+        )
 
         if region_data.invert:
             text = "-" + str(region_data.index)
@@ -91,16 +123,47 @@ def _draw_regions(image: cv2.typing.MatLike, image_path: str, regions_data: list
             text = str(region_data.index)
 
         if flip:
-            cv2.putText(draw_image, text, (x + region_width - random.randint(-5, 5), y + region_height-random.randint(-5, 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            cv2.putText(
+                draw_image,
+                text,
+                (
+                    x + region_width - random.randint(-5, 5),
+                    y + region_height - random.randint(-5, 5),
+                ),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                color,
+                2,
+            )
         else:
-            cv2.putText(draw_image, text, (x - random.randint(-5, 5), y - random.randint(-5, 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            cv2.putText(
+                draw_image,
+                text,
+                (x - random.randint(-5, 5), y - random.randint(-5, 5)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                color,
+                2,
+            )
 
         if subregion_draw:
-            cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}.subregion.{index}.png"), region_data.region)
+            cv2.imwrite(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(image_path)),
+                    f"{highlight_name}.subregion.{index}.png",
+                ),
+                region_data.region,
+            )
 
-    cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}.png"), draw_image)
+    cv2.imwrite(
+        os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}.png"),
+        draw_image,
+    )
 
-def _find_regions(image : cv2.typing.MatLike, image_path : str, draw : bool, highlight_name : str, invert = True) -> list['RegionData']:
+
+def _find_regions(
+    image: cv2.typing.MatLike, image_path: str, draw: bool, highlight_name: str, invert=True
+) -> list["RegionData"]:
     """
     Finds regions in the given image.
     """
@@ -115,25 +178,27 @@ def _find_regions(image : cv2.typing.MatLike, image_path : str, draw : bool, hig
 
         # Adding small padding to image for slight context and better search accuracy
         margin = 5
-        region_data = image[max(0, y - margin) : y + h + margin, max(0, x - margin) : x + w + margin]
+        region_data = image[
+            max(0, y - margin) : y + h + margin, max(0, x - margin) : x + w + margin
+        ]
 
         unique_colors_count, pct = _count_colours(region_data)
         # also get a greyscale version of the region for the other attributes
         # (see paper by Evdoxios Baratis and Euripides G.M. Petrakis why this is)
 
-        if (region_data.size == 0):
+        if region_data.size == 0:
             continue
 
         r_grey = cv2.cvtColor(region_data, cv2.COLOR_BGR2GRAY)
 
         # Image info
-        mean = np.mean(r_grey, axis = None)
-        std = np.std(r_grey, axis = None)
-        skew = ss.skew(r_grey, axis = None)
-        kurtosis = ss.kurtosis(r_grey, axis = None)
-        entropy = ss.entropy(r_grey, axis = None)
+        mean = np.mean(r_grey, axis=None)
+        std = np.std(r_grey, axis=None)
+        skew = ss.skew(r_grey, axis=None)
+        kurtosis = ss.kurtosis(r_grey, axis=None)
+        entropy = ss.entropy(r_grey, axis=None)
 
-        #Otsu threshold
+        # Otsu threshold
         otsu = 0
         if invert:
             otsu = cv2.threshold(r_grey, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[0]
@@ -141,8 +206,8 @@ def _find_regions(image : cv2.typing.MatLike, image_path : str, draw : bool, hig
             otsu = cv2.threshold(r_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[0]
 
         # Energy
-        _, (cH, cV, cD) = dwt2(r_grey.T, 'db1')
-        energy = (cH**2 + cV**2 + cD**2).sum()/r_grey.size
+        _, (ch, cv, cd) = dwt2(r_grey.T, "db1")
+        energy = (ch**2 + cv**2 + cd**2).sum() / r_grey.size
 
         if math.isnan(energy):
             energy = 0.0
@@ -152,21 +217,74 @@ def _find_regions(image : cv2.typing.MatLike, image_path : str, draw : bool, hig
         occupied_bins = np.count_nonzero(int_hist)
 
         if draw:
-            cv2.rectangle(draw_image,(x-margin,y-margin),(x+w+margin,y+h+margin),(0,0,255),1)
-
+            cv2.rectangle(
+                draw_image,
+                (x - margin, y - margin),
+                (x + w + margin, y + h + margin),
+                (0, 0, 255),
+                1,
+            )
 
         if len(hierarchy) > 0:
-            regions_data.append(RegionData(region_data, index, x, y, unique_colors_count, pct, hierarchy[0][index], invert, mean, std, skew, kurtosis, entropy, otsu, energy, occupied_bins))
+            regions_data.append(
+                RegionData(
+                    region_data,
+                    index,
+                    x,
+                    y,
+                    unique_colors_count,
+                    pct,
+                    hierarchy[0][index],
+                    invert,
+                    mean,
+                    std,
+                    skew,
+                    kurtosis,
+                    entropy,
+                    otsu,
+                    energy,
+                    occupied_bins,
+                )
+            )
         else:
-            regions_data.append(RegionData(region_data, index, x, y, unique_colors_count, pct, [-2, -2, -2, -2], invert, mean, std, skew, kurtosis, entropy, otsu, energy, occupied_bins))
-   
+            regions_data.append(
+                RegionData(
+                    region_data,
+                    index,
+                    x,
+                    y,
+                    unique_colors_count,
+                    pct,
+                    [-2, -2, -2, -2],
+                    invert,
+                    mean,
+                    std,
+                    skew,
+                    kurtosis,
+                    entropy,
+                    otsu,
+                    energy,
+                    occupied_bins,
+                )
+            )
+
     if draw:
-        cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}.png"), draw_image)
+        cv2.imwrite(
+            os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}.png"),
+            draw_image,
+        )
         main_logger.debug("Wrote image highlighting the regions to: " + highlight_name)
 
     return regions_data
 
-def _get_contours(image : cv2.typing.MatLike, invert : bool, image_path: str, draw : bool, highlight_name : str = "Highlight"):
+
+def _get_contours(
+    image: cv2.typing.MatLike,
+    invert: bool,
+    image_path: str,
+    draw: bool,
+    highlight_name: str = "Highlight",
+):
     """
     Calculates contours and their hierarchy.
     """
@@ -175,7 +293,12 @@ def _get_contours(image : cv2.typing.MatLike, invert : bool, image_path: str, dr
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     if draw:
-        cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-0-grey.png"),  processed_img)
+        cv2.imwrite(
+            os.path.join(
+                os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-0-grey.png"
+            ),
+            processed_img,
+        )
 
     main_logger.debug("Thresholding the image")
     if invert:
@@ -184,36 +307,63 @@ def _get_contours(image : cv2.typing.MatLike, invert : bool, image_path: str, dr
         cv2.threshold(processed_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU, processed_img)
 
     if draw:
-        cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-1-tresh.png"),  processed_img)
+        cv2.imwrite(
+            os.path.join(
+                os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-1-tresh.png"
+            ),
+            processed_img,
+        )
 
     main_logger.debug("Dilating")
-    processed_img = cv2.dilate(processed_img, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 5)), iterations = 1)
+    processed_img = cv2.dilate(
+        processed_img, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 5)), iterations=1
+    )
 
     if draw:
-        cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-2-dilating.png"),  processed_img)
+        cv2.imwrite(
+            os.path.join(
+                os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-2-dilating.png"
+            ),
+            processed_img,
+        )
 
     main_logger.debug("Morphing to merge close area's")
-    processed_img = cv2.morphologyEx(processed_img, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
+    processed_img = cv2.morphologyEx(
+        processed_img, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    )
 
     if draw:
-        cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-3-inter.png"),  processed_img)
+        cv2.imwrite(
+            os.path.join(
+                os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-3-inter.png"
+            ),
+            processed_img,
+        )
 
     main_logger.debug("Eroding")
-    processed_img = cv2.erode(processed_img, cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4)), iterations = 1)
+    processed_img = cv2.erode(
+        processed_img, cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4)), iterations=1
+    )
 
     if draw:
-        cv2.imwrite(os.path.join(os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-4-eroding.png"),  processed_img)
+        cv2.imwrite(
+            os.path.join(
+                os.path.dirname(os.path.realpath(image_path)), f"{highlight_name}-4-eroding.png"
+            ),
+            processed_img,
+        )
 
     main_logger.debug("Finding contours")
     contours, hierarchy = cv2.findContours(processed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     return contours, hierarchy
 
-def _validate_regions(regions_data : list[RegionData]) -> list[RegionData]:
+
+def _validate_regions(regions_data: list[RegionData]) -> list[RegionData]:
     """
     Filters on the regions, by removing overlapping regions.
     """
-    regions_of_interest : list[RegionData] = []
+    regions_of_interest: list[RegionData] = []
 
     # TODO check significance of enumeration order, since important (better, more complete) regions may be skipped due to overlap
     for index, region_data in enumerate(regions_data):
@@ -226,9 +376,13 @@ def _validate_regions(regions_data : list[RegionData]) -> list[RegionData]:
 
             # TODO check proper overlap condition, this will only detect overlap on one quarter
             region_height2, region_width2, _ = region_data2.region.shape
-            if region_data.x >= region_data2.x and (region_data.x + region_width <= region_data2.x + region_width2):
+            if region_data.x >= region_data2.x and (
+                region_data.x + region_width <= region_data2.x + region_width2
+            ):
                 # On x axis region1 is contained within region2
-                if region_data.y >= region_data2.y and (region_data.y + region_height <= region_data2.y + region_height2):
+                if region_data.y >= region_data2.y and (
+                    region_data.y + region_height <= region_data2.y + region_height2
+                ):
                     # On y axis region 1 is contained within region2
                     continue
 
@@ -236,24 +390,27 @@ def _validate_regions(regions_data : list[RegionData]) -> list[RegionData]:
 
     return regions_of_interest
 
-class DrawingFlags(Enum):
 
+class DrawingFlags(Enum):
     FLAG_NO_DRAW = 0
-    """Tells the function to **NOT** draw any debugging images.""" 
+    """Tells the function to **NOT** draw any debugging images."""
 
     FLAG_DRAW = 1
-    """Tells the function to draw the regions on the original image.""" 
+    """Tells the function to draw the regions on the original image."""
 
     FLAG_DRAW_RECURSIVE = 2
-    """Tells the function to draw the regions and the changes applied to the image.""" 
+    """Tells the function to draw the regions and the changes applied to the image."""
 
     FLAG_SUBREGION_DRAW = 3
-    """Tells the function to draw the regions and subregions."""	
+    """Tells the function to draw the regions and subregions."""
 
     FLAG_DRAW_ALL = 4
     """Tells the function to draw all the regions, subregions and changes made to the image."""
 
-def find_regions(image_path : str, draw_flag = DrawingFlags.FLAG_DRAW, highlight_name = "Highlight") -> tuple[list[RegionData], tuple[int, int, int]]:
+
+def find_regions(
+    image_path: str, draw_flag=DrawingFlags.FLAG_DRAW, highlight_name="Highlight"
+) -> tuple[list[RegionData], tuple[int, int, int]]:
     """
     Finds all the regions of interest in the image and returns the data of the image.
 
@@ -264,20 +421,38 @@ def find_regions(image_path : str, draw_flag = DrawingFlags.FLAG_DRAW, highlight
     """
 
     draw = draw_flag != DrawingFlags.FLAG_NO_DRAW
-    recursive_draw = draw_flag == DrawingFlags.FLAG_DRAW_RECURSIVE or draw_flag == DrawingFlags.FLAG_DRAW_ALL
-    subregion_draw = draw_flag == DrawingFlags.FLAG_SUBREGION_DRAW or draw_flag == DrawingFlags.FLAG_DRAW_ALL
+    recursive_draw = (
+        draw_flag == DrawingFlags.FLAG_DRAW_RECURSIVE or draw_flag == DrawingFlags.FLAG_DRAW_ALL
+    )
+    subregion_draw = (
+        draw_flag == DrawingFlags.FLAG_SUBREGION_DRAW or draw_flag == DrawingFlags.FLAG_DRAW_ALL
+    )
 
     main_logger.debug("Loading image: " + image_path)
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
     img_data = (_count_colours(image), image.shape[0], image.shape[1])
 
-    regions_data = _find_regions(image, image_path, recursive_draw, highlight_name = f"{highlight_name}.allregions.inverted", invert = True)
-    regions_data += _find_regions(image, image_path, recursive_draw, highlight_name = f"{highlight_name}.allregions.not_inverted", invert = False)
+    regions_data = _find_regions(
+        image,
+        image_path,
+        recursive_draw,
+        highlight_name=f"{highlight_name}.allregions.inverted",
+        invert=True,
+    )
+    regions_data += _find_regions(
+        image,
+        image_path,
+        recursive_draw,
+        highlight_name=f"{highlight_name}.allregions.not_inverted",
+        invert=False,
+    )
 
     regions_of_interest = _validate_regions(regions_data)
 
     if draw:
-        _draw_regions(image, image_path, regions_of_interest, f"{highlight_name}.allregions", subregion_draw)
+        _draw_regions(
+            image, image_path, regions_of_interest, f"{highlight_name}.allregions", subregion_draw
+        )
 
     return regions_of_interest, img_data
