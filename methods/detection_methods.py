0f3081e18116c4579831e03d5790366d045368f7
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-03-01 13:05:31 +0100

    Added some notes

--- methods/detection_methods.py
+++ methods/detection_methods.py
@@ -2,10 +2,13 @@ from abc import ABC, abstractmethod
 from enum import Enum, auto
 
 
+# TODO this class might change, be deleted or moved
 class DetectionMethods(Enum):
     ReverseImageSearch = auto()
     Test = auto()
 
+
+# TODO this class might change, be deleted or moved
 class DetectionMethod(ABC):
     @abstractmethod
     def test(self):
