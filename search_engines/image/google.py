26f0b64998b891140d023429e0d06a56d2dd57ce
TPGamesNL <pet.teun.03@gmail.com>
2024-03-05 14:29:09 +0100

    Formatting and small docs changes

--- search_engines/image/google.py
+++ search_engines/image/google.py
@@ -6,9 +6,8 @@ from requests_html import HTML, HTMLResponse, HTMLSession
 from search_engines.image.base import ReverseImageSearchEngine
 from utils.google import accept_all_cookies, check_blockage
 
-DEFAULT_RENDER_TIMEOUT = 3.0
-
 SEARCH_RESULT_SELECTOR = ".Vd9M6 a"
+
 
 class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
     """A :class:`ReverseImageSearchEngine` configured for google.com"""
@@ -16,7 +15,7 @@ class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
     htmlsession: HTMLSession = None
 
     def __init__(self):
-        super().__init__('Google')
+        super().__init__("Google")
 
     def make_request(self, region) -> HTMLResponse:
         """Sends an HTML POST request to Google Lens with the given region.
@@ -25,7 +24,7 @@ class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
         ----------
         url: str
             The URL to send the request to.
-        
+
         Returns
         -------
         requests_html.HTMLResponse
@@ -33,25 +32,27 @@ class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
         """
 
         url = "https://lens.google.com/v3/upload?hl=nl&re=df&st=1709288673579&vpw=790&vph=738&ep=gisbubb"
-        
+
         # Encode multipart data
         png_img = cv2.imencode(".png", region)[1]
-        multipart_data = {"encoded_image": ("temp.png", png_img, 'image/png')}
-        
+        multipart_data = {"encoded_image": ("temp.png", png_img, "image/png")}
+
         # Add User Agent header
         headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
         }
 
         self.main_logger.info("Sending request to Google Lens")
         try:
             # Make POST request
-            html_res: HTMLResponse = self.htmlsession.post(url, files=multipart_data, headers=headers)
+            html_res: HTMLResponse = self.htmlsession.post(
+                url, files=multipart_data, headers=headers
+            )
 
             if html_res.status_code != 200:
                 self.main_logger.error(f"Google Lens returned status code {html_res.status_code}")
         except Exception as e:
-            raise IOError('Error while sending request to Google Lens') from e
+            raise IOError("Error while sending request to Google Lens") from e
 
         check_blockage(html_res)
 
@@ -71,7 +72,9 @@ class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
         # New text search
         matches = html.find(SEARCH_RESULT_SELECTOR)
         if len(matches) == 0:
-            raise ValueError("Given HTML wasn't in a known search result format, as no search results were found.")
+            raise ValueError(
+                "Given HTML wasn't in a known search result format, as no search results were found."
+            )
 
         for match in matches:
             for link in match.absolute_links:
@@ -86,7 +89,7 @@ class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
         """
         if self.htmlsession is not None:
             return
-        
+
         self.main_logger.info("Starting HTML session")
         htmlsession = HTMLSession()
 
@@ -94,22 +97,24 @@ class GoogleReverseImageSearchEngine(ReverseImageSearchEngine):
 
         self.htmlsession = htmlsession
 
-    def query(self, region) -> Iterator['str']:
+    def query(self, region) -> Iterator["str"]:
         self.create_htmlsession()
 
         # Execute search query
         html_res = self.make_request(region)
 
         html = html_res.html
-        
+
         # Extract URLs from query results
         extracted_urls = self.extract_search_result_urls(html)
 
         if len(extracted_urls) == 0:
-            self.main_logger.info('Reverse image search query reached end')
+            self.main_logger.info("Reverse image search query reached end")
             return
 
-        self.main_logger.info(f'Reverse image search query gave {len(extracted_urls)} results so far')
+        self.main_logger.info(
+            f"Reverse image search query gave {len(extracted_urls)} results so far"
+        )
 
         # Yield all retrieved URLs
         for extracted_url in extracted_urls:
--- search_engines/text/google.py
+++ search_engines/text/google.py
@@ -6,12 +6,11 @@ from requests_html import HTML, HTMLResponse, HTMLSession
 from search_engines.text.base import TextSearchEngine
 from utils.google import accept_all_cookies, check_blockage
 
-DEFAULT_RENDER_TIMEOUT = 3.0
-
 SEARCH_RESULT_SELECTOR = ".egMi0.kCrYT"
 NEXT_PAGE_SELECTOR = ".nBDE1b.G5eFlf"
 
 # TODO improve cookie page detection
+
 
 class GoogleTextSearchEngine(TextSearchEngine):
     """A :class:`TextSearchEngine` configured for google.com"""
@@ -19,7 +18,7 @@ class GoogleTextSearchEngine(TextSearchEngine):
     htmlsession: HTMLSession = None
 
     def __init__(self):
-        super().__init__('Google')
+        super().__init__("Google")
 
     def construct_search_url(self, query: str) -> str:
         """
@@ -28,29 +27,27 @@ class GoogleTextSearchEngine(TextSearchEngine):
         return "https://www.google.com/search?q=" + quote_plus(query)
 
     def make_request(self, url: str) -> HTMLResponse:
-        """Sends an HTML GET request to the given URL and renders it.
+        """Sends an HTML GET request to the given URL.
 
         Parameters
         ----------
         url: str
             The URL to send the request to.
-        
+
         Returns
         -------
         requests_html.HTMLResponse
             The HTML response from the request.
         """
         self.main_logger.info(f"Sending request to: {url}")
-        
+
         try:
             # Make GET request
             html_res: HTMLResponse = self.htmlsession.get(url)
 
-            self.main_logger.info(
-                f"Request returned status code: {html_res.status_code}"
-            )
+            self.main_logger.info(f"Request returned status code: {html_res.status_code}")
         except Exception as e:
-            raise IOError(f'Error while sending request to Google to URL `{url}`') from e
+            raise IOError(f"Error while sending request to Google to URL `{url}`") from e
 
         check_blockage(html_res)
 
@@ -72,8 +69,10 @@ class GoogleTextSearchEngine(TextSearchEngine):
         if len(matches) == 0:
             if "heeft geen overeenkomstige documenten opgeleverd" in html.text:
                 return []
-            
-            raise ValueError("Given HTML wasn't in a known search result format, as no search results were found.")
+
+            raise ValueError(
+                "Given HTML wasn't in a known search result format, as no search results were found."
+            )
 
         for match in matches:
             for link in match.absolute_links:
@@ -104,7 +103,9 @@ class GoogleTextSearchEngine(TextSearchEngine):
 
         if (first_page and len(matches) != 1) or (not first_page and len(matches) != 2):
             if len(matches) == 0:
-                self.main_logger.warning("Could not find next page button in `HTML` object, either indicative of no search results or of Google interface changes")
+                self.main_logger.warning(
+                    "Could not find next page button in `HTML` object, either indicative of no search results or of Google interface changes"
+                )
             return None
 
         # Allow either 1 (next page button) or 2 (previous page + next page) a elements.
@@ -114,7 +115,7 @@ class GoogleTextSearchEngine(TextSearchEngine):
         links = matches[0].absolute_links
         if len(links) != 1:
             raise ValueError(f"Next page button has non-one links (it has {len(links)}): {links}")
-        
+
         # Return the absolute link
         return list(links)[0]
 
@@ -124,7 +125,7 @@ class GoogleTextSearchEngine(TextSearchEngine):
         """
         if self.htmlsession is not None:
             return
-        
+
         self.main_logger.info("Starting HTML session")
         htmlsession = HTMLSession()
 
@@ -132,7 +133,7 @@ class GoogleTextSearchEngine(TextSearchEngine):
 
         self.htmlsession = htmlsession
 
-    def query(self, text: str) -> Iterator['str']:
+    def query(self, text: str) -> Iterator["str"]:
         self.create_htmlsession()
 
         # Construct initial search query URL
@@ -142,15 +143,15 @@ class GoogleTextSearchEngine(TextSearchEngine):
             # Execute search query
             html_res = self.make_request(url)
             html = html_res.html
-            
+
             # Extract URLs from query results
             extracted_urls = self.extract_search_result_urls(html)
 
             if len(extracted_urls) == 0:
-                self.main_logger.info('Text search query reached end')
+                self.main_logger.info("Text search query reached end")
                 break
 
-            self.main_logger.info(f'Text search query gave {len(extracted_urls)} results so far')
+            self.main_logger.info(f"Text search query gave {len(extracted_urls)} results so far")
 
             # Yield all retrieved URLs
             for extracted_url in extracted_urls:
@@ -159,5 +160,5 @@ class GoogleTextSearchEngine(TextSearchEngine):
             # Continue on with the results from the next page
             url = self.get_next_page_link(html, first_page=first_page)
             first_page = False
-            
-            self.main_logger.info(f'Visiting next page with URL `{url}`')
+
+            self.main_logger.info(f"Visiting next page with URL `{url}`")
