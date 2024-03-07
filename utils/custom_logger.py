f9345b9bb18da1acf54ca5d476c1842c328226ac
TPGamesNL <pet.teun.03@gmail.com>
2024-03-07 12:35:22 +0100

    Change logger name again, Add some comments

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 import time
 
-main_logger: logging.Logger = logging.getLogger('main')
+main_logger: logging.Logger = logging.getLogger('zpd')
 """
 The main logger to be used by the program.
 """
@@ -10,31 +10,41 @@ The main logger to be used by the program.
 if not os.path.exists("log"):
     os.makedirs("log")
 
+# Allow all log levels
 main_logger.setLevel(logging.DEBUG)
 
-formatter_c = logging.Formatter(
-    "%(asctime)s  [\033[93m%(pathname)s:%(lineno)d\033[0m]  %(levelname)s: %(message)s"
-)
-formatter_f = logging.Formatter(
-    "%(asctime)s  [%(pathname)s:%(lineno)d]  %(levelname)s: %(message)s"
-)
+def setup():
+    global main_logger
 
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(formatter_c)
-main_logger.addHandler(console_handler)
+    formatter_c = logging.Formatter(
+        "%(asctime)s  [\033[93m%(pathname)s:%(lineno)d\033[0m]  %(levelname)s: %(message)s"
+    )
+    formatter_f = logging.Formatter(
+        "%(asctime)s  [%(pathname)s:%(lineno)d]  %(levelname)s: %(message)s"
+    )
 
-file_handler = logging.FileHandler(f"log/python-{time.time()}.log", mode="w")
-file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(formatter_f)
-main_logger.addHandler(file_handler)
+    # Add console log handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter_c)
+    main_logger.addHandler(console_handler)
 
-file_handler_error = logging.FileHandler(f"log/error-{time.time()}.log", mode="w")
-file_handler_error.setLevel(logging.ERROR)
-file_handler_error.setFormatter(formatter_f)
-main_logger.addHandler(file_handler_error)
+    # Add main file log handler
+    file_handler = logging.FileHandler(f"log/python-{time.time()}.log", mode="w")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter_f)
+    main_logger.addHandler(file_handler)
 
-file_handler_debug = logging.FileHandler(f"log/debug-{time.time()}.log", mode="w")
-file_handler_debug.setLevel(logging.DEBUG)
-file_handler_debug.setFormatter(formatter_f)
-main_logger.addHandler(file_handler_debug)
+    # Add extra file log handler at ERROR level
+    file_handler_error = logging.FileHandler(f"log/error-{time.time()}.log", mode="w")
+    file_handler_error.setLevel(logging.ERROR)
+    file_handler_error.setFormatter(formatter_f)
+    main_logger.addHandler(file_handler_error)
+
+    # Add extra file log handler at DEBUG level
+    file_handler_debug = logging.FileHandler(f"log/debug-{time.time()}.log", mode="w")
+    file_handler_debug.setLevel(logging.DEBUG)
+    file_handler_debug.setFormatter(formatter_f)
+    main_logger.addHandler(file_handler_debug)
+
+setup()
