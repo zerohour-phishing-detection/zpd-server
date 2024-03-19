ec9e5540ed8fe3b35f62b7c0d5467213a38acd2d
TPGamesNL <pet.teun.03@gmail.com>
2024-03-19 15:33:23 +0100

    Improve documentation, clean code up a bit

--- logo_finders/vision_logo_detection.py
+++ logo_finders/vision_logo_detection.py
@@ -5,47 +5,62 @@ from typing import AsyncIterator
 from google.cloud import vision
 
 from logo_finders.base import LogoFinder
+from search_engines.text.base import TextSearchEngine
 from search_engines.text.google import GoogleTextSearchEngine
 
+# Set environment variable for the Google Cloud Service Account Key
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ".gcloud_creds.json"
 
-features = [vision.Feature(type_=vision.Feature.Type.LOGO_DETECTION)]
+# The features that we want to annotate images with, i.e. just logo detection
+FEATURES = [vision.Feature(type_=vision.Feature.Type.LOGO_DETECTION)]
 
 class VisionLogoDetection(LogoFinder):
+	"""
+	A `LogoFinder` that uses Google Cloud Vision's Logo Detection API
+	to find the brand names of logos in an image, and then uses text search engines
+	to find associated domains.
+	"""
 	client: vision.ImageAnnotatorAsyncClient
+	text_search: TextSearchEngine
 
-	def __init__(self):
+	def __init__(self, text_search: TextSearchEngine = GoogleTextSearchEngine()):
+		"""
+		Initiates the instance and connects to the Google Cloud API.
+
+		Note: make sure you call this within context of the same event loop,
+		as it will error otherwise.
+		"""
 		super().__init__("vision_logo_detection")
 		self.client = vision.ImageAnnotatorAsyncClient()
+		self.text_search = text_search
 
 	async def find(self, img_path) -> AsyncIterator[str]:
 		"""Detects logos in the file."""
 
+		# Read image
 		with open(img_path, "rb") as image_file:
 			content = image_file.read()
-
 		image = vision.Image(content=content)
 
-		request = vision.AnnotateImageRequest(image=image, features=features)
+		# Make request to Google Cloud and get response
+		request = vision.AnnotateImageRequest(image=image, features=FEATURES)
 		responses = await self.client.batch_annotate_images(requests=[request])
 		response = responses.responses[0]
 
+		# Check for error
 		if response.error.message:
 			raise Exception(
 				"{}\nFor more info on error messages, check: "
 				"https://cloud.google.com/apis/design/errors".format(response.error.message)
 			)
 
-		logo_names = response.logo_annotations
+		# Collect results
+		logo_names = [logo.description for logo in response.logo_annotations]
 
-		g = GoogleTextSearchEngine()
+		self.logger.info(f'Detected {len(logo_names)} logo(s): {logo_names}')
 
-		self.logger.info(f'Detected {len(logo_names)} logos')
-
-		for logo in logo_names:
-			logo_name = logo.description
-			self.logger.info(f'Found logo origin: {logo_name}')
-			
-			for logo_url in itertools.islice(g.query(logo_name), 7):
+		for logo_name in logo_names:
+			# Look up each logo brand name on the text search engine to get a URL from it
+			# Take top 3 search results
+			for logo_url in itertools.islice(self.text_search.query(logo_name), 3):
 				yield logo_url
-		
