710baf65b0b2c9df0d83ff3ede14a0dbfd78d5ef
ronalddenouden <104371933+ronalddenouden@users.noreply.github.com>
2024-03-19 14:01:45 +0100

    Add ProcessGroup to util async Threads
    ProcessGroup handles keeping track of the tasks scheduled and returning them to you. Interacts with ThreadWorker.

--- utils/async_threads.py
+++ utils/async_threads.py
@@ -1,3 +1,25 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+
+class ProcessGroup:
+    """
+        Handles interaction with ThreadWorker by keeping track of scheduled tasks and returning them.
+    """
+    scheduled_processes: list[asyncio.Future]
+    worker: "ThreadWorker"
+
+    def __init__(self, worker: "ThreadWorker"):
+        self.scheduled_processes = []
+        self.worker = worker
+
+    def schedule(self, task):
+        scheduled_process = self.worker.run_task(task)
+        self.scheduled_processes.append(scheduled_process)
+
+    def get_scheduled_processes(self) -> list[asyncio.Future]: # Look into which future to use, this one or concurrent.future
+        return self.scheduled_processes
+
 class ThreadWorker: #Look at how many threads and what lifetime should be of thread.
     """
         Thread worker handling running tasks concurrently. ProcessGroup should be used to schedule tasks and get the results.
