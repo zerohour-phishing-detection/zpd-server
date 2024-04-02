f3212ef01f5e8d17b19ac71a37621a64c271a07b
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-04-02 16:41:34 +0200

    Explained better what homebrew is

--- settings/dst.py
+++ settings/dst.py
@@ -2,7 +2,7 @@ from settings import Settings
 
 
 class DSTSettings(Settings):
-    # Which logo finder to use, 0 for `reverse_logo_region_search`, 1 for `vision_logo_detection`
+    # Which logo finder to use, `homebrew` is an alias for `reverse_logo_region_search` and `google_vision` for `vision_logo_detection`
     LOGO_FINDERS: dict[str:int] = {"homebrew": 0, "google_vision": 1}
 
     logo_finder: int
