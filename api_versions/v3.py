93153722a5b3c3373e62cbd41baa219675f6fbb8
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-04-04 13:15:56 +0200

    Auto generation of the UUID cookie

--- api_versions/v3.py
+++ api_versions/v3.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify, request
+import uuid
+
+from flask import Blueprint, g, jsonify, request
 
 import detection
 from detection import DetectionData
@@ -18,6 +20,22 @@ session_storage = detection.session_storage
 settings_storage = detection.settings_storage
 
 
+@v3.before_request
+def before_request():
+    g.uuid = request.cookies.get("uuid")
+
+    if g.uuid is None:
+        g.uuid = str(uuid.uuid4())
+        logger.info(f"Generating new uuid: {g.uuid}")
+
+
+@v3.after_request
+def after_request(response):
+    if request.cookies.get("uuid") is None:
+        response.set_cookie("uuid", g.uuid)
+
+    return response
+
 @v3.route("/ping", methods=["GET"])
 def ping():
     return ""
@@ -26,20 +44,18 @@ def ping():
 # TODO: Use jsonify instead for consistency
 @v3.route("/check", methods=["POST"])
 def check():
-    uuid = request.cookies.get("uuid")
     json = request.get_json()
     data = DetectionData.from_json(json)
 
-    return detection.check(uuid, data).to_json_str()
+    return detection.check(g.uuid, data).to_json_str()
 
 
 @v3.route("/state", methods=["POST"])
 def get_state():
-    uuid = request.cookies.get("uuid")
     json = request.get_json()
     url = json["url"]
 
-    session = session_storage.get_session(uuid, url)
+    session = session_storage.get_session(g.uuid, url)
     status = session.get_state()
 
     if status is None:
@@ -52,22 +68,19 @@ def get_state():
 
 @v3.route("/settings", methods=["GET"])
 def get_settings():
-    uuid = request.cookies.get("uuid")
-
-    settings = settings_storage.get_settings(uuid)
+    settings = settings_storage.get_settings(g.uuid)
 
     if settings is None:
         return ("", 404)
 
-    return jsonify(settings_storage.get_settings(uuid))
+    return jsonify(settings)
 
 
 @v3.route("/settings", methods=["POST"])
 def set_settings():
-    uuid = request.cookies.get("uuid")
     json = request.get_json()
 
-    if not settings_storage.set_settings(uuid, json):
+    if not settings_storage.set_settings(g.uuid, json):
         return ("", 400)
 
     return ("", 200)
@@ -75,9 +88,8 @@ def set_settings():
 
 @v3.route("/capabilities", methods=["GET"])
 def get_available_capabilities():
-    uuid = request.cookies.get("uuid")
 
-    logger.info(f"Capabilites request from uuid: {uuid}")
+    logger.info(f"Capabilites request from uuid: {g.uuid}")
     result = {
         "detection_methods": list(DETECTION_METHODS.keys()),
         "decision_strategies": list(DECISION_STRATEGIES.keys()),
